0eb3dbacabfd00789223eb901abd95290d1cabe7
julien <julien.calabro@gmail.com>
2020-04-23 00:45:35 +0200

    a

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args){
 
         int a = 10;
-        System.out.println("hello world " + a);
+        System.out.println("hello world " + a + " c'est bon la?");
     }
 }

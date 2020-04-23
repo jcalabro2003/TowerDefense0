619c8dc7db1dd0a0cf948035620f44b273c5f231
LittleSamourai <samiirtiouririne@gmail.com>
2020-04-23 13:44:29 +0200

    Commit mASSAGE

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args){
 
         int a = 10;
-        System.out.println("hello world " + a + " c'est bon la?");
+        System.out.println("hTEST ULTIMATE " + a + " C BON LA FDP?");
         int b = 3; // wow
-        System.out.println("dernier test");
+        System.out.println("TEST ULTIME");
         boolean cloned = true;
     }
 }
